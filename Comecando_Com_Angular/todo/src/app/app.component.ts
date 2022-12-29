bfdf2b2e70be36b2a17df2ddc8f3147dc8461009
Danilo Silva <danilo.silva@msn.com>
2022-12-28 23:08:57 -0300

    Variáveis e Tipos

--- Comecando_Com_Angular/todo/src/app/app.component.ts
+++ Comecando_Com_Angular/todo/src/app/app.component.ts
@@ -5,4 +5,17 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  // Variável pública
+  // Any -> Qualquer Coisa
+  // [] -> Um Array (Lista)
+  // Se não inicializar é undefined, se inicializar é vazio
+  public todos: any[] = [];
+
+  // Construtor chamado toda vez que meu componente inicia
+  constructor() {
+    this.todos.push("Passear com o Cachorro");
+    this.todos.push("Ir ao Supermercado");
+    this.todos.push("Cortar o Cabelo");
+  }
+}
